b503e201445506e47acdc3ed27de1a60f77f30a2
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-03 14:53:25 -0300

    perf: improving the code

--- api/index.js
+++ api/index.js
@@ -5,4 +5,8 @@ const config = require('config')
 
 app.use(bodyParser.json())
 
-app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!rs'))
