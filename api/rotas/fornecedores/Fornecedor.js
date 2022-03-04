5b934c27ed60950635ac352177b181e3f882c93c
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-04 16:06:04 -0300

    feat:creating the verb post

--- api/rotas/fornecedores/Fornecedor.js
+++ api/rotas/fornecedores/Fornecedor.js
@@ -23,6 +23,36 @@ class Fornecedor {
         this.dataAtualizacao = resultado.dataAtualizacao
         this.versao = resultado.versao
     }
+
+    async carregar () {
+        const encontrado = await TabelaFornecedor.pegarPorId(this.id)
+        this.empresa = encontrado.empresa
+        this.email = encontrado.email
+        this.categoria = encontrado.categoria
+        this.dataCriacao = encontrado.dataCriacao
+        this.dataAtualizacao = encontrado.dataAtualizacao
+        this.versao = encontrado.versao
+    }
+
+    async atualizar () {
+        await TabelaFornecedor.pegarPorId(this.id)
+        const campos = ['empresa', 'email', 'categoria']
+        const dadosParaAtualizar = {}
+
+        campos.forEach((campo) => {
+            const valor = this[campo]
+
+            if (typeof valor === 'string' && valor.length > 0) {
+                dadosParaAtualizar[campo] = valor
+            }
+        })
+
+        if (Object.keys(dadosParaAtualizar).length === 0) {
+            throw new Error('Não foram fornecidos dados para atualizar!')
+        }
+
+        await TabelaFornecedor.atualizar(this.id, dadosParaAtualizar)
+    }
 }
 
 module.exports = Fornecedor
