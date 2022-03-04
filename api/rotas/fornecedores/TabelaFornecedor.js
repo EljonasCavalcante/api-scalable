e8f46c0323546ae099a23806a65962a136504ff3
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-04 16:08:39 -0300

    feat:creating table routes for post supplier

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -2,9 +2,30 @@ const Modelo = require('./ModeloTabelaFornecedor')
 
 module.exports = {
     listar () {
-    return Modelo.findAll()
+        return Modelo.findAll()
     },
     inserir (fornecedor) {
         return Modelo.create(fornecedor)
+    },
+    async pegarPorId (id) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if (!encontrado) {
+            throw new Error('Fornecedor não encontrado')
+        }
+
+        return encontrado
+    },
+    atualizar (id, dadosParaAtualizar) {
+        return Modelo.update(
+            dadosParaAtualizar,
+            {
+                where: { id: id }
+            }
+        )
     }
 }
