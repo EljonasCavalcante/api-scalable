b628f8a3cca09d7095e8864a62b15e4274bb6bfb
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-11 15:20:35 -0300

    feat: update

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
 
 app.use(bodyParser.json())
 
@@ -18,7 +19,7 @@ app.use((erro, requisicao, resposta, proximo) => {
         status = 404
     }
 
-    if (erro instanceof CampoInvalido) {
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
     }
     resposta.status(status)
--- api/rotas/fornecedores/Fornecedor.js
+++ api/rotas/fornecedores/Fornecedor.js
@@ -1,5 +1,6 @@
 const TabelaFornecedor = require('./TabelaFornecedor')
 const CampoInvalido = require('../../erros/CampoInvalido')
+const DadosNaoFornecidos = require('../../erros/DadosNaoFornecidos')
 
 class Fornecedor {
     constructor ({ id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao }) {
@@ -50,7 +51,7 @@ class Fornecedor {
         })
 
         if (Object.keys(dadosParaAtualizar).length === 0) {
-            throw new Error('Não foram fornecidos dados para atualizar!')
+            throw new DadosNaoFornecidos()
         }
 
         await TabelaFornecedor.atualizar(this.id, dadosParaAtualizar)
