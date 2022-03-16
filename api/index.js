921b21ea7aefd6ff72d949af538adff7262d1180
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-15 23:03:13 -0300

    feat:Creating files js error

--- api/index.js
+++ api/index.js
@@ -5,8 +5,27 @@ const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formartosAceitos = require('./Serializador').formartosAceitos
 
 app.use(bodyParser.json())
+
+app.use((requisicao, resposta, proximo) => {
+    let formatoRequisitado = requisicao.header('Accept')
+
+    if(formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    if (formartosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+})
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
@@ -21,6 +40,10 @@ app.use((erro, requisicao, resposta, proximo) => {
 
     if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
+    }
+
+    if(erro instanceof ValorNaoSuportado) {
+        status = 406
     }
     resposta.status(status)
     resposta.send(
