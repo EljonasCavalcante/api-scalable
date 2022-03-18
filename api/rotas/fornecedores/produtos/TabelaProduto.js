6031ac4d78f9333b37c0812c40c8973db2fe5c8d
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-17 23:44:41 -0300

    feat:creating product routes

---
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -0,0 +1,12 @@
+const Modelo = require('./ModeloTabelaProduto')
+
+
+module.exports = {
+    listar (idFornecedor) {
+        return Modelo.findAll({
+            where: {
+                fornecedor: idFornecedor
+            }
+        })
+    }
+}
