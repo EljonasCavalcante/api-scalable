2f6986c5e231d64350a6c06182a590ef24b1e1ad
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-24 10:38:03 -0300

    feat:creating error response if product not found and frontend integration

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -1,3 +1,4 @@
+const { purge } = require('.')
 const Tabela = require('./TabelaProduto')
 
 class Produto {
@@ -39,6 +40,16 @@ class Produto {
 
     apagar () {
         return Tabela.remover(this.id, this.fornecedor)
+    }
+
+    async carregar () {
+        const produto = await Tabela.pegarPorId(this.id, this.fornecedor)
+        this.titulo = produto.titulo
+        this.preco = produto.preco
+        this.estoque = produto.estoque
+        this.dataCriacao = produto.dataCriacao
+        this.dataAtualizacao = produto.dataAtualizacao
+        this.versao = produto.versao
     }
 }
 
--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -19,5 +19,20 @@ module.exports = {
                 fornecedor: idFornecedor
             }
         })
+    },
+    async pegarPorId (idProduto, idFornecedor) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            },
+            raw: true
+        })
+
+        if( ! encontrado) {
+            throw new Error('Produto não encontrado!')
+        }
+
+        return encontrado
     }
 }
