0ea852c8f472397b7f1ee859257f80903c82c3be
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-03-23 22:38:47 -0300

    feat: adding product route

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router({ mergeParams: true})
 const Tabela = require('./TabelaProduto')
+const Produto =require('./Produto')
 
 roteador.get('/', async (requisicao, resposta) => {
     const produtos = await Tabela.listar(requisicao.params.idFornecedor)
@@ -8,6 +9,15 @@ roteador.get('/', async (requisicao, resposta) => {
     )
 })
 
-const roteadorProdutos = require('../produtos') 
+roteador.post('/', async (requisicao, resposta) => {
+    const idFornecedor = resiquicao.params.idFornecedor
+    const corpo = requisicao.body
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor})
+    const produto = new Produto(dados)
+    await produto.criar()
+    resposta.status(201)
+    resposta.send(produto)
+})
+
 
 module.exports = roteador
